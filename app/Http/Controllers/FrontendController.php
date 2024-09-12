d3f597b136b1b5eacb44415bc1a9e9f1e4590227
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-09-12 22:48:31 +0600

    frontend add tags

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     function index(){
         $categories = Category::all();
+        $tags = Tag::all();
         return view('frontend.index',[
             'categories'=> $categories,
+            'tags'=>$tags,
         ]);
     }
 }
